acacd5bad75d343bd8aaefe2aa0186a9d416e037
Phittinanphu <64110105@kmitl.ac.th>
2025-02-07 18:40:56 +0700

    home page

---
+++ src/app/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import TitleSection from "./Titles";
+import FeatureSection from "./Feature";
+import Navbar from "./Navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Navbar />
+      <TitleSection />
+      <FeatureSection />
+      
+    </div>
+  );
+};
+
+export default Home;
+
+
