33c07a8229c9d0bc2db1a03856ea6a85a845b18f
Phittinanphu <64110105@kmitl.ac.th>
2025-03-03 16:23:13 +0700

    BackToHome button

--- src/app/components/SubmitSuccess.tsx
+++ src/app/components/SubmitSuccess.tsx
@@ -1,7 +1,6 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import Link from "next/link";
 
 const SubmitSucessScreen: React.FC = () => {
   const router = useRouter();
@@ -9,6 +8,10 @@ const SubmitSucessScreen: React.FC = () => {
 
   const HandleViewAnswer = () => {
     router.push("/submission_success"); 
+  };
+
+  const HandleBackToHome = () => {
+    router.push("/main"); 
   };
 
   return (
@@ -27,14 +30,23 @@ const SubmitSucessScreen: React.FC = () => {
       <div className="w-646 h-329 relative z-10 bg-white p-10 rounded-3xl shadow-lg text-center max-w-lg w-full">
         <h1 className="text-4xl font-bold text-black mt-2">Submission Successful</h1>
 
-        {/* Start Button */}
-        
-        <button
-        onClick={HandleViewAnswer}
-        className="w-227 h-50 mt-6 bg-red-600 text-white text-lg font-semibold px-8 py-3 rounded-lg hover:bg-red-700 transition duration-300"
-        >
-        View Answer
-        </button>
+        {/* Button */}
+        <div className="flex gap-2 justify-center">
+            <button
+                onClick={HandleBackToHome}
+                className="w-227 h-50 mt-6 bg-blue-600 text-white text-lg font-semibold px-8 py-3 rounded-lg hover:bg-blue-700 transition duration-300"
+            >
+                Back To Home
+            </button>
+            <button
+                onClick={HandleViewAnswer}
+                className="w-227 h-50 mt-6 bg-green-400 text-white text-lg font-semibold px-8 py-3 rounded-lg hover:bg-green-700 transition duration-300"
+            >
+                View Answer
+            </button>
+            </div>
+
+
         
       </div>
     </div>
